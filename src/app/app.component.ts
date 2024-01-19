f53ceeacb1f1b61d3a65a205f20907130188f769
crystalchavez99 <redfire1205@hotmail.com>
2024-01-19 09:49:47 -0800

    understand angular more

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'first-project';
+  name: string = "Crystal Chavez";
+  itemList: string[] = ['item1', 'item2', 'item3', 'item4'];
+  rectangle = {
+    length: 5,
+    width: 6,
+    area: function () {
+      return this.length * this.width;
+    }
+  }
 }
